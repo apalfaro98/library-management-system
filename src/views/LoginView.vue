c0da7e87fe525240f7dcf37211af5a9c17c5fb70
Angelica Perez <apalfaro98@gmail.com>
2022-02-26 21:55:31 -0500

    Creada pantalla del login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fondo"></div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<style scoped>
+.fondo {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url(@/assets/books-background.png);
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
